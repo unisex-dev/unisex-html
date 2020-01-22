505d30580f0b0acaa21c690caf6788da4750c662
Tomas Gajardo Gutierrez <tooomas821@gmail.com>
2020-01-22 00:22:50 -0300

    cambios 12:00- 22 enero 2020

--- js/unisex.js
+++ js/unisex.js
@@ -160,11 +160,9 @@ $(document).ready(function() {
         breakpoints: {
             640: {
                 slidesPerView: 1,
-                spaceBetween: 5,
             },
             768: {
                 slidesPerView: 3,
-                spaceBetween: 1,
             },
             1024: {
                 slidesPerView: 3,
